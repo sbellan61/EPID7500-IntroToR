43654b44d9f28a71e92fe7a0fa815fee8bf8a2b8
Steven Bellan <stevenbellan@s172-20-246-h28.paws.uga.edu>
2017-08-14 13:49:39 -0400

    master & starter file separated

--- Day 1/DHSanalysisMaster.R
+++ Day 1/DHSanalysisMaster.R
@@ -4,6 +4,23 @@ getwd()
 setwd('~/Documents/R Repos/EPID7500-IntroToR/Day 1/')
 getwd()
 ls()
+## Hiding this from them
+load(url('https://github.com/sbellan61/Bellan-2013-Lancet-DHS-analysis-/raw/master/alldhs.Rdata'))
+dhs <- as.tibble(dat)
+dhs <- mutate(dhs, mpos = as.numeric(ser %in% 1:2), fpos = as.numeric(ser %in% c(1,3))) %>%
+    select(uid, ds, group, mpos, fpos, ser, tms, tfs, tmar, tint, mage, fage, mlsp, flsp,
+           mevtest, fevtest) %>%
+    rename(userID=uid, dataset=ds, country=group, coupleSerostatus=ser,
+           maleMonthSexDebut = tms, femaleMonthSexDebut = tfs,
+           monthMarriage = tmar, monthInterview = tint,
+           maleAge = mage, femaleAge = fage,
+           malePartners = mlsp, femalePartners = flsp,
+           maleEvTest = mevtest, femaleEvTest = fevtest)
+dhs
+save(dhs, file='alldhs.Rda')
+rm(list=ls(all=T))
+ls()
+## End hiding
 rm(list=ls(all=T))
 load(url('https://github.com/sbellan61/EPID7500-IntroToR/raw/gh-pages/Day%201/alldhs.Rda'))
 dhs
@@ -16,18 +33,25 @@ str(dhs)
 ## What do you want to look at in this data to start exploring it?
 
 ## Cross-tabulation
+xtabs(~mpos + fpos, dhs)
+xtabs(~mpos + country, dhs)
+xtabs(~ser, dhs)
 
+ggplot(data=filter(dhs, flsp<95 & mlsp<95) , aes(x=flsp, y=mlsp)) + 
+  geom_point(aes(colour=mpos), alpha = .5) + 
+  facet_wrap(~group, nrow = 4) + geom_smooth(method='lm') +
+  xlab('female lifetime sexual partners') +
+  ylab('male lifetime sexual partners') 
 
+dhs %>% distinct(country)
+dhs %>% distinct(dataset)
+dhs %>% distinct(dataset) %>% print(n=40)
 
-
-
-
-
-
-
-
-
-
+dhs %>%
+  mutate(tmar = tmar/12+1900) %>%
+  filter(group=='Rwanda') %>%
+  select(group, tmar, mlsp, flsp) %>%
+  slice(1:10)
 
 
 ##################################################
